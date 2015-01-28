677a9c4ede8a891536e4cf5a685e034c0ed7cf67
CrazyCrud <constantin.lehenmeier@stud.uni-regensburg.de>
2015-01-28 14:07:12 +0100

    Libraries added

--- index.php
+++ index.php
@@ -3,8 +3,16 @@
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/vendor/normalize.css">
+	<link rel="stylesheet" type="text/css" href="css/vendor/foundation.css">
+	<link rel="stylesheet" type="text/css" href="css/vendor/overrides.css">
+	<link rel="stylesheet" type="text/css" href="css/modules/base.css">
+	<link rel="stylesheet" type="text/css" href="css/sections/home.css">
 </head>
 <body>
 
 </body>
+<script type="text/javascript" src="js/vendor/jquery.js"></script>
+<script type="text/javascript" src="js/vendor/tappy.js"></script>
+<script type="text/javascript" src="js/sections/home.js"></script>
 </html>
